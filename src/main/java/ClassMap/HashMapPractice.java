727792a7d956b7ff7fc0fd5080e312db2058ede0
Sofiane Acherir <132833027+sofus1985@users.noreply.github.com>
2025-01-14 14:17:31 -0600

    add geolocalisation

--- src/main/java/ClassMap/HashMapPractice.java
+++ src/main/java/ClassMap/HashMapPractice.java
@@ -1,19 +1,61 @@
 package ClassMap;
-
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapPractice {
 
 
     public static void main(String[] args) {
 
-        HashMap map = new HashMap();
-        map.put("city", "philadelphia");
-        map.put("zipCode", "19234");
-        System.out.println(  map);
+        Map<Object, Object> map1 = new HashMap<>();
+
+        map1.put("city", "philadelphia");
+        map1.put("zipCode", 19147);
+        System.out.println("this is teh map :" + map1);
 
 
-      }
+        Object[] entriesArray1 = map1.entrySet().toArray(new Map.Entry[0]);
+
+        for (Object entry1 : entriesArray1) {
+            System.out.println("Key-Value Pairs Array: : " + entry1);
+        }
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "alex");
+        map.put(2, "lora");
+
+        //Convert and Print Keys as an Array
+        Integer[] keysArray = map.keySet().toArray(new Integer[0]);
+        System.out.println("Keys Array: " + Arrays.toString(keysArray));
+
+        //Convert and Print Values as an Array
+        String[] valuesArray = map.values().toArray(new String[0]);
+        System.out.println("Values Array: " + Arrays.toString(valuesArray));
+
+        //Convert and Print Key-Value Pairs as an Array
+        String[] entriesArray = map.entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + "=" + entry.getValue())
+                .toArray(String[]::new);
+
+        System.out.println("Entries Array: " + Arrays.toString(entriesArray));
+
+
+        // Convert map to an array of Map.Entry
+        Map.Entry<Integer, String>[] entryArray = map.entrySet().toArray(new Map.Entry[0]);
+        // Print the array
+        for (Map.Entry<Integer, String> entry : entryArray) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+
+        }
+
 
     }
+}
+
+
+
+
+
 
---
+++ src/main/java/Geolocation_localisation/Geolocation.java
@@ -0,0 +1,74 @@
+package Geolocation_localisation;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+public class Geolocation {
+
+
+    @Test
+
+    public void geolocation(){
+       ChromeDriver driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.manage().deleteAllCookies();
+
+      DevTools devTools = driver.getDevTools();
+      devTools.createSession();
+
+        HashMap<String, Object> coordinates = new HashMap<>();
+        coordinates.put("latitude", 40);
+        coordinates.put("longitude", 3);
+        coordinates.put("accuracy", 1);
+
+      driver.executeCdpCommand("Emulation.setGeolocationOverride", coordinates);
+
+      driver.get("http://google.com");
+
+      // click in google search
+      driver.findElement(By.name("q")).sendKeys("netflix", Keys.ENTER);
+
+
+      // wit to load
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".LC20lb.MBeuO.DKV0Md")));
+      // click in netflix
+        driver.findElements(By.cssSelector(".LC20lb.MBeuO.DKV0Md")).get(0).click();
+
+
+
+      // get the text in netflix
+      String title = driver.findElement(By.xpath("//h1[@class=' default-ltr-cache-jmnaey euy28770']")).getText();
+        System.out.println(title);
+
+
+        driver.quit();
+
+    }
+
+
+}
+//https://chromedevtools.github.io/devtools-protocol/tot/Emulation/
+
+/*
+The DevTools class comes from the Selenium DevTools package, which is part of the Selenium 4.x suite. This package allows integration with the Chrome DevTools Protocol (CDP), enabling advanced interactions with the Chrome browser.
+
+Specifically, the main classes related to DevTools in Selenium are found in the org.openqa.selenium.devtools package.
+
+Here's a brief overview of some key classes:
+
+DevTools - Represents the DevTools object you use to create sessions and send commands.
+
+HasDevTools - An interface that allows a WebDriver instance to access DevTools.
+
+DevToolsSession - Represents a devtools session, which is used to send commands and receive events.
+ */
